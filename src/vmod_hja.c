d1d2828be29ab4d3a0a9d4a15e2b53cb336ed80e
Helmut Januschka <helmut@januschka.com>
2017-06-27 14:55:23 +0200

    format

--- src/vmod_hja.c
+++ src/vmod_hja.c
@@ -1,19 +1,19 @@
 #include "config.h"
 
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /* need vcl.h before vrt.h for vmod_evet_f typedef */
+#include "cache/cache.h"
 #include "vcl.h"
 #include "vrt.h"
-#include "cache/cache.h"
 
-#include "vtim.h"
 #include "vcc_hja_if.h"
+#include "vtim.h"
 
 const size_t infosz = 64;
-char	     *info;
+char *info;
 
 /*
  * handle vmod internal state, vmod init/fini and/or varnish callback
@@ -24,95 +24,92 @@ char	     *info;
  */
 
 int __match_proto__(vmod_event_f)
-event_function(VRT_CTX, struct vmod_priv *priv, enum vcl_event_e e)
+    event_function(VRT_CTX, struct vmod_priv *priv, enum vcl_event_e e)
 {
-	char	   ts[VTIM_FORMAT_SIZE];
-	const char *event = NULL;
+  char ts[VTIM_FORMAT_SIZE];
+  const char *event = NULL;
 
-	(void) ctx;
-	(void) priv;
+  (void)ctx;
+  (void)priv;
 
-	switch (e) {
-	case VCL_EVENT_LOAD:
-		info = malloc(infosz);
-		if (! info)
-			return (-1);
-		event = "loaded";
-		break;
-	case VCL_EVENT_WARM:
-		event = "warmed";
-		break;
-	case VCL_EVENT_COLD:
-		event = "cooled";
-		break;
-	case VCL_EVENT_DISCARD:
-		free(info);
-		return (0);
-		break;
-	default:
-		return (0);
-	}
-	AN(event);
-	VTIM_format(VTIM_real(), ts);
-	snprintf(info, infosz, "vmod_hja %s at %s", event, ts);
+  switch (e) {
+    case VCL_EVENT_LOAD:
+      info = malloc(infosz);
+      if (!info) return (-1);
+      event = "loaded";
+      break;
+    case VCL_EVENT_WARM:
+      event = "warmed";
+      break;
+    case VCL_EVENT_COLD:
+      event = "cooled";
+      break;
+    case VCL_EVENT_DISCARD:
+      free(info);
+      return (0);
+      break;
+    default:
+      return (0);
+  }
+  AN(event);
+  VTIM_format(VTIM_real(), ts);
+  snprintf(info, infosz, "vmod_hja %s at %s", event, ts);
 
-	return (0);
+  return (0);
 }
 
 VCL_STRING
 vmod_info(VRT_CTX)
 {
-	(void) ctx;
+  (void)ctx;
 
-	return (info);
+  return (info);
 }
 
-
-static void first_path_to_lower(char * c) {
+static void first_path_to_lower(char *c)
+{
   int track = 0;
-  char * url = c;
-  for(; *c; c++) {
-    if(*c == '/' && track == 0) {
+  char *url = c;
+  for (; *c; c++) {
+    if (*c == '/' && track == 0) {
       track = 1;
       continue;
     }
-    if(track == 1) {
+    if (track == 1) {
       *c = tolower(*c);
     }
-    if(*c == '/' && track == 1) {
+    if (*c == '/' && track == 1) {
       break;
     }
-
   }
   // Check if we have a / at the end and remove it
-  
-  if(url[strlen(url)-1] == '/') {
-    url[strlen(url)-1] = '\0';
+
+  if (url[strlen(url) - 1] == '/') {
+    url[strlen(url) - 1] = '\0';
   }
-  
 }
 
 VCL_STRING
 vmod_first_folder_lower(VRT_CTX, VCL_STRING name)
 {
-	char *p;
-	unsigned u, v;
+  char *p;
+  unsigned u, v;
 
-  char * orig_string;
+  char *orig_string;
   orig_string = strdup(name);
   first_path_to_lower(orig_string);
 
-	u = WS_Reserve(ctx->ws, 0); /* Reserve some work space */
-	p = ctx->ws->f;		/* Front of workspace area */
-	v = snprintf(p, u, "%s", orig_string);
+  u = WS_Reserve(ctx->ws, 0); /* Reserve some work space */
+  p = ctx->ws->f;             /* Front of workspace area */
+  v = snprintf(p, u, "%s", orig_string);
   free(orig_string);
-	v++;
-	if (v > u) {
-		/* No space, reset and leave */
-		WS_Release(ctx->ws, 0);
-		return (NULL);
-	}
-	/* Update work space with what we've used */
-	WS_Release(ctx->ws, v);
-	return (p);
+  v++;
+  if (v > u) {
+    /* No space, reset and leave */
+    WS_Release(ctx->ws, 0);
+    return (NULL);
+  }
+  /* Update work space with what we've used */
+  WS_Release(ctx->ws, v);
+  return (p);
 }
